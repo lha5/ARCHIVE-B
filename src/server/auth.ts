f4c552d584601c5e45d0599a86305c9e41b806a4
lha <hyeahnlee5@gmail.com>
2024-01-04 20:49:40 +0900

    feat: zustand, daisyUI 추가 및 discord 로그인 삭제

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,9 +1,8 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { type GetServerSidePropsContext } from 'next';
 import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
 
-import { env } from '@/env';
+// import { env } from '@/env';
 import { db } from '@/server/db';
 
 /**
@@ -44,10 +43,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    // DiscordProvider({
+    //   clientId: env.DISCORD_CLIENT_ID,
+    //   clientSecret: env.DISCORD_CLIENT_SECRET,
+    // }),
     /**
      * ...add more providers here.
      *
