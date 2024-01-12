6fdd8f898f0bdd905f2abf9bc70e392b493c8d24
lha <hyeahnlee5@gmail.com>
2024-01-12 18:45:11 +0900

    feat: add a new file - setting/index.tsx

---
+++ src/pages/setting/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Setting() {
+  return (
+    <>
+      <Head>
+        <title>ARCHIVE-B | 관리자 설정</title>
+      </Head>
+      <main>
+        <h1 className="text-2xl">SETTING PAGE</h1>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Signin() {
+  return (
+    <>
+      <Head>
+        <title>ARCHIVE-B | 로그인</title>
+      </Head>
+      <main>
+        <h1 className="text-2xl">SIGN-IN PAGE</h1>
+      </main>
+    </>
+  );
+}
